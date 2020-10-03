14e9c1c8e9f464ca58f82fed5dde08a9b252eaf0
Joshua Rotenberg <joshua.rotenberg@gmail.com>
2020-10-02 21:05:27 -0400

    refactored randomize and zipcode

--- randomAddressGenerator.js
+++ randomAddressGenerator.js
@@ -1,7 +1,3 @@
-
-
-// street number, street name, city name, state name and zip code
-
 const randomGenerator = () => {
 
     const direction = ['N', 'NE', 'NW', 'W', 'S', 'SE', 'SW', 'E'];
@@ -9,20 +5,25 @@ const randomGenerator = () => {
     const type = ['Blvd', 'Rd', "Ln", "St"];
     const city = ["Boulevard Gardens", "Broadview Park", "Chula Vista", "Coconut Creek", "Cooper City", "Coral Springs", "Dania Beach", "Davie", "Deerfield Beach", "Fern Crest Village", "Fort Lauderdale", "Franklin Park", "Hallandale Beach", "Hillsboro Beach", "Hillsboro Pines", "Hollywood", "Lake Forest", "Lauderdale Lakes", "Lauderdale-by-the-Sea", "Lauderhill", "Lazy Lake", "Lighthouse Point", "Margate", "Miramar", "North Lauderdale", "Oakland Park", "Parkland", "Pembroke Park", "Pembroke Pines", "Plantation", "Pompano Beach", "Roosevelt Gardens", "Sea Ranch Lakes", "Southwest Ranches", "Sunrise", "Tamarac", "Washington Park", "West Park", "Weston"];
     const state_abbrev = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+
     let streetNum = Math.floor(Math.random() * 10000);
-    let streetZip = Math.floor(Math.random()*90000) + 10000;
 
-    let streetDirection = direction[Math.floor(Math.random() * direction.length)];
-    let streetName = name[Math.floor(Math.random() * name.length)];
-    let streetType = type[Math.floor(Math.random() * type.length)];
-    let streetCity = city[Math.floor(Math.random() * city.length)];
-    let streetState = state_abbrev[Math.floor(Math.random() * state_abbrev.length)];
+    const singleZipNum = () => {  
+        return Math.floor(Math.random() * 9); 
+    };
 
-    //figure out how to make this dry => array[Math.floor(Math.random() * array.length)];
+    const zipcode = () => {
+        return(`${singleZipNum()}${singleZipNum()}${singleZipNum()}${singleZipNum()}${singleZipNum()}`);
+    };
 
-    return(`${streetNum} ${streetDirection} ${streetName} ${streetType}, ${streetCity} ${streetState}, ${streetZip}`);
+    const randomize = (name) => {  
+        return name[Math.floor(Math.random() * name.length)]; 
+    }  
+
+    return(`${streetNum} ${randomize(direction)} ${randomize(name)} ${randomize(type)}, ${randomize(city)} ${randomize(state_abbrev)}, ${zipcode()}`);
 
 }
 
 console.log(randomGenerator());
+
 
