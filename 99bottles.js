1f9e9779e62c8c70fc0d4ad73f7662716ae1e582
Joshua Rotenberg <joshua.rotenberg@gmail.com>
2020-10-05 08:04:02 -0400

    renaming varibales

--- 99bottles.js
+++ 99bottles.js
@@ -1,9 +1,9 @@
 // link to lyrics didn't work so I used https://en.wikipedia.org/wiki/99_Bottles_of_Beer
 
-const FavBottlesofBeer = (beer) => {
+const FavBottlesOfBeer = (beer) => {
 
     for (let i = 99; i >= 0; i--) {
-        let bottleCount = i - 1;
+        let oneLessBottle = i - 1;
 
         if (i == 0) {
             return(`No more bottles of ${beer} on the wall, no more bottles of ${beer}.
@@ -13,16 +13,16 @@ const FavBottlesofBeer = (beer) => {
              Take one down, pass it around, no more bottles of ${beer} on the wall...`);
         } else if (i < 3) {
            return(`${i} bottles of ${beer} on the wall, ${i} bottles of ${beer}.
-                Take one down, pass it around, ${bottleCount} bottle of ${beer} on the wall...`);
+                Take one down, pass it around, ${oneLessBottle} bottle of ${beer} on the wall...`);
         } else {
             console.log(`${i} bottles of ${beer} on the wall, ${i} bottles of ${beer}.`);
             if (i % 5 == 0) {
-                console.log( ` If one of those bottles should happen to fall, ${bottleCount} bottles of ${beer} on the wall...`);
+                console.log( ` If one of those bottles should happen to fall, ${oneLessBottle} more bottles of ${beer} on the wall...`);
             } else {
-                console.log( ` Take one down, pass it around, ${bottleCount} bottles of ${beer} on the wall...`);
+                console.log( ` Take one down, pass it around, ${oneLessBottle} more bottles of ${beer} on the wall...`);
             }
         }
     }
 }
 
-console.log(FavBottlesofBeer("Corona"));
+console.log(FavBottlesOfBeer("Corona"));
