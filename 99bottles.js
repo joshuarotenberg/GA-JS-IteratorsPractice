a1e985733e6ba8fb340e16ef01b35e13fcd1ff21
Joshua Rotenberg <joshua.rotenberg@gmail.com>
2020-10-05 08:00:31 -0400

    use your fav bottle of beer

--- 99bottles.js
+++ 99bottles.js
@@ -1,24 +1,28 @@
 // link to lyrics didn't work so I used https://en.wikipedia.org/wiki/99_Bottles_of_Beer
 
+const FavBottlesofBeer = (beer) => {
 
-for (let i = 99; i >= 0; i--) {
-    let bottleCount = i - 1;
+    for (let i = 99; i >= 0; i--) {
+        let bottleCount = i - 1;
 
-    if (i == 0) {
-        console.log("No more bottles of beer on the wall, no more bottles of beer.");
-        console.log(" We've taken them down and passed them around; now we're drunk and passed out!");
-    } else if (i < 2) {
-        console.log(`${i} bottle of beer on the wall, ${i} bottle of beer.`);
-        console.log(" Take one down, pass it around, no more bottles of beer on the wall...");
-    } else if (i < 3) {
-        console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
-        console.log(" Take one down, pass it around, " + bottleCount + " bottle of beer on the wall...");
-    } else {
-        console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
-        if (i % 5 == 0) {
-            console.log( ` If one of those bottles should happen to fall, ${bottleCount} bottles of beer on the wall...`);
+        if (i == 0) {
+            return(`No more bottles of ${beer} on the wall, no more bottles of ${beer}.
+                We've taken them down and passed them around; now we're drunk and passed out!`);
+        } else if (i < 2) {
+            return(`${i} bottle of ${beer} on the wall, ${i} bottle of ${beer}.
+             Take one down, pass it around, no more bottles of ${beer} on the wall...`);
+        } else if (i < 3) {
+           return(`${i} bottles of ${beer} on the wall, ${i} bottles of ${beer}.
+                Take one down, pass it around, ${bottleCount} bottle of ${beer} on the wall...`);
         } else {
-            console.log( ` Take one down, pass it around, ${bottleCount} bottles of beer on the wall...`);
+            console.log(`${i} bottles of ${beer} on the wall, ${i} bottles of ${beer}.`);
+            if (i % 5 == 0) {
+                console.log( ` If one of those bottles should happen to fall, ${bottleCount} bottles of ${beer} on the wall...`);
+            } else {
+                console.log( ` Take one down, pass it around, ${bottleCount} bottles of ${beer} on the wall...`);
+            }
         }
     }
 }
+
+console.log(FavBottlesofBeer("Corona"));
